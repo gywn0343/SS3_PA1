ef130a40141bb5a625e18dca3552770c118ae199
gywn0343 <gywn0343@gmail.com>
2018-11-27 07:29:16 -0800

    Magazine added. E_book still waiting for former notice. no more usage of struct info.

--- resource.cpp
+++ resource.cpp
@@ -4,137 +4,195 @@
 #include "resource.h"
 using namespace std;
 
-
-void Resource::print()
+void Book::add_resource(string _name)
 {
-	int i;
-	for(i=0;i<name.size();i++)
-	{
-		cout << name.at(i) << ": " << state.at(i) << " ";
-		cout << (date.at(i)).year << "/" << (date.at(i)).month << "/" << (date.at(i)).day;
-		cout << " ~ ";
-		cout << (due_date.at(i)).year << "/" << (due_date.at(i)).month << "/" << (due_date.at(i)).day;
-		cout << endl;
-	}
-	cout << "------------------" << endl;
-}
-
-void Resource::set_date(string _date, int due)
-{
-	(date.at(location)).year = stoi(_date.substr(0, 2));
-	(date.at(location)).month = stoi(_date.substr(3, 2));
-	(date.at(location)).day = stoi(_date.substr(6, 2));
-
-	struct info tmp = date.at(location);
-	due--;
-	if(tmp.day + due > 30)
-	{
-		if(tmp.month + 1 > 12)
-		{
-			(due_date.at(location)).year = tmp.year + 1;
-			(due_date.at(location)).month = 1;
-		}
-		else
-		{
-			(due_date.at(location)).month = tmp.month + 1;
-			(due_date.at(location)).year = tmp.year;
-		}
-		(due_date.at(location)).day = (tmp.day + due) % 30;
-	}
-	else
-	{
-		(due_date.at(location)).day = tmp.day + due;
-		(due_date.at(location)).month = tmp.month;
-		(due_date.at(location)).year = tmp.year;
-	}
-}
-
-void Resource::struct_to_string(struct info due, string &ret_date)
-{
-	ret_date.push_back(due.year / 10 + '0');
-	ret_date.push_back(due.year % 10 + '0');
-	ret_date.push_back('/');
-	ret_date.push_back(due.month / 10 + '0');
-	ret_date.push_back(due.month % 10 + '0');
-	ret_date.push_back('/');
-	ret_date.push_back(due.day / 10 + '0');
-	ret_date.push_back(due.day % 10 + '0');
-}
-void Resource::add_resource(string _name)
-{
-	struct info tmp;
-	tmp.year = 0;
-	tmp.month = 0;
-	tmp.day = 0;
+	INFO data_tmp;
+	data_tmp.mem_name = "";
+	data_tmp.state = true;
+	data_tmp.date = "";
+	data_tmp.due_date = "";
 	name.push_back(_name);
-	state.push_back(true);
-	date.push_back(tmp);
-	due_date.push_back(tmp);
-	mem_name.push_back("");
+	data.push_back(data_tmp);
 }
+void Magazine::add_resource(string _name)
+{
+	INFO *data_tmp = new INFO[N_MGZ];
+	int i;
+	for(i=0;i<N_MGZ;i++)
+	{
+		data_tmp[i].mem_name = "";
+		data_tmp[i].state = true;
+		data_tmp[i].date = "";
+		data_tmp[i].due_date = "";
+		data_tmp[i].month = "";
+	}
+	name.push_back(_name);
+	data.push_back(data_tmp);
+}
+/*void E_book::add_resource(string _name, int size)
+{
+	INFO data_tmp;
+	data_tmp.mem_name = "";
+	data_tmp.state = true;
+	data_tmp.date = "";
+	data_tmp.due_date = "";
+	data_tmp.size = size;
+	name.push_back(_name);
+	data.push_back(data_tmp);
+}*/
 int Resource::isInLibrary(string _name)
 {
 	for(int i=0;i<name.size();i++)
 	{
 		if(name.at(i) == _name)
 		{
-			location = i;
-			return 0;
+			return i;
 		}
 	}
 	return -1;
 }
-int Resource::isAvailable(string member_name, string _date, string& ret_date)
+int Book::isAvailable(string member_name, string _date, string& ret_date)
 {
-	if(state.at(location) == true)
+	if(data.at(location).state == true)
 	{
 		return 0;
 	}
 	else
 	{
-		if(mem_name.at(location) == member_name)
+		if(data.at(location).mem_name == member_name)
 		{
-			struct_to_string(date.at(location), ret_date);
+			ret_date = data.at(location).date;
 			return 4;
 		}
 		else
 		{
-			struct_to_string(due_date.at(location), ret_date);
+			ret_date = data.at(location).due_date;
 			return 5;
 		}
 	}
 }
-void Resource::final_state(bool in, int due, string member_name, string _date)
+
+int Magazine::isAvailable(string member_name, string _date, string& ret_date, string month)
 {
-	state.at(location) = in;
-	if(member_name != "") 
-		mem_name.at(location) = member_name;
-	if(_date != "")
-		set_date(_date, due);
+	int i, tmp;
+	
+
+
+	for(i=0;i<N_MGZ;i++)
+	{
+		if(data.at(location)[i].month == month)
+		{
+			if(data.at(location)[i].mem_name == member_name) 
+			{
+				ret_date = data.at(location)[i].date;
+				return 4;
+			}
+			else
+			{
+				ret_date = data.at(location)[i].due_date;
+				return 5;
+			}
+		}
+	}
+
+	return 0;
 }
 
-int Resource::isGoodReturn(string member_name, string _date)
+void Resource::set_date(string _date, int due, string& date, string& due_date)
 {
-	if(mem_name.at(location) != member_name)
-	{
-		return 3;
-	}
+	date = _date;	
+	due_date = "";
+
+	int tyear = stoi(_date.substr(0, 2));
+	int tmonth = stoi(_date.substr(3, 2));
+	int tday = stoi(_date.substr(6, 2));
+	due--;
 	int year, month, day;
-	year = stoi(_date.substr(0, 2));
-	month = stoi(_date.substr(3, 2));
-	day = stoi(_date.substr(6, 2));
-	if(year > (due_date.at(location)).year)
-		return 7;
-	if(month > (due_date.at(location)).month)
-		return 7;
-	if(day > (due_date.at(location)).day)
-		return 7;
+	if(tday + due > 30)
+	{
+		if(tmonth + 1 > 12)
+		{
+			year = tyear + 1;
+			month = 1;
+		}
+		else
+		{
+			month = tmonth + 1;
+			year = tyear;
+		}
+		day = (tday + due) % 30;
+	}
+	else
+	{
+		day = tday + due;
+		month = tmonth;
+		year = tyear;
+	}
+	if(year < 10) due_date += "0";
+	due_date += to_string(year);
+	due_date += "/";
+	if(month < 10) due_date += "0";
+	due_date += to_string(month);
+	due_date += "/";
+	if(day < 10) due_date += "0";
+	due_date += to_string(day);
+}
+void Book::final_state(bool in, int due, string member_name, string _date)
+{
+	data.at(location).state = in;
+	if(member_name != "") 
+		data.at(location).mem_name = member_name;
+	if(_date != "")
+		set_date(_date, due, data.at(location).date, data.at(location).due_date);
+}
+void Magazine::final_state(bool in, int due, string member_name, string _date)
+{
+	int i;
+
+	if(in == false) // Borrow
+	{
+		for(i=0;i<N_MGZ;i++)
+		{
+			if(data.at(location)[i].month == "")
+			{
+				data.at(location)[i].state = in;
+				data.at(location)[i].mem_name = member_name;
+				set_date(_date, due, data.at(location)[i].date, data.at(location)[i].due_date);
+				data.at(location)[i].month = month_rec;
+				return;
+			}
+		}
+	}
+	else // Return
+	{
+		for(i=0;i<N_MGZ;i++)
+		{
+			if(data.at(location)[i].month == month_rec)
+			{
+				data.at(location)[i].state = in;
+				data.at(location)[i].mem_name = "";
+				data.at(location)[i].month = "";
+				return;
+			}
+		}
+	}
+}
+
+int Resource::isGoodReturn(string _date, string due_date)
+{
+	int i;
+	for(i=0;i<_date.size();i++)
+	{
+		if(_date.at(i) < due_date.at(i)) return 0;
+		if(_date.at(i) > due_date.at(i)) return 7;
+	}
 	return 0;
 }
 int Book::do_op(string B, string _name, string mem_name, string now, string &ret_date)
 {
 	int ret;
-	if(isInLibrary(_name) == -1) return 1;
+	if((ret = isInLibrary(_name)) == -1) return 1;
+	else location = ret;
 	if(B == "B")
 	{
 		ret = isAvailable(mem_name, now, ret_date);
@@ -142,8 +200,96 @@ int Book::do_op(string B, string _name, string mem_name, string now, string &ret
 	}
 	else 
 	{
-		ret = isGoodReturn(mem_name, now);
-		if(ret == 7 || ret == 3) return ret;
+		if(data.at(location).mem_name != mem_name)
+			return 3;
+		ret = isGoodReturn(now, data.at(location).due_date);
+		if(ret == 7) return ret;
 	}
 	return 0;
 }
+/*int E_book::do_op(string B, string _name, string mem_name, string now, string &ret_date)
+{
+	int ret;
+	if((ret = isInLibrary(_name)) == -1) return 1;
+	else location = ret;
+	if(B == "B")
+	{
+		ret = isAvailable(mem_name, now, ret_date);
+		if(ret == 4 || ret == 5) return ret; 
+	}
+	else 
+	{
+		if(data.at(location).mem_name != mem_name)
+			return 3;
+		ret = isGoodReturn(now, data.at(location).due_date);
+		if(ret == 7) return ret;
+	}
+	return 0;
+}*/
+int Magazine::check_month(string _date, string month)
+{
+	int n_year = stoi(_date.substr(0, 2));
+	int n_month = stoi(_date.substr(3, 2));
+	int b_year = stoi(month.substr(0, 2));
+	int b_month = stoi(month.substr(3, 2));
+
+	if(n_year < b_year)
+		return 1;          // no such month's magazine (requesting future mag)
+	else if(n_year > b_year)
+	{
+		if(n_year == b_year + 1)	
+		{
+			if(n_month > b_month) return 1; // req expired mag
+		}
+		else return 1; // requesting expired magazine
+	}
+	else
+	{
+		if(n_month < b_month) // requesting future mag
+			return 1;
+	}
+}
+int Magazine::do_op(string B, string _name, string mem_name, string now, string &ret_date)
+{
+	int ret;
+	int s, e;
+
+	string s_b = "[";
+	string e_b = "]";
+	s = _name.find(s_b);
+	e = _name.find(e_b);
+	string month = _name.substr(s + 1, e - s - 1);
+
+	month_rec = month;
+	ret = check_month(now, month);
+	if(ret == 1) return ret;
+
+	_name = _name.substr(0, s);
+	if((ret = isInLibrary(_name)) == -1) return 1;
+	else location = ret;
+
+
+	int i;
+	if(B == "B")
+	{
+		ret = isAvailable(mem_name, now, ret_date, month);
+		if(ret == 4 || ret == 5) return ret; 
+	}
+	else 
+	{
+		for(i=0;i<N_MGZ;i++)
+		{
+			if(data.at(location)[i].month == month) break;
+		}
+		if(data.at(location)[i].mem_name != mem_name)
+			return 3;
+		ret = isGoodReturn(now, data.at(location)[i].due_date);
+		if(ret == 7) return ret;
+	}
+	return 0;
+}
+
+
+
+
+
--- resource.h
+++ resource.h
@@ -4,40 +4,66 @@
 using namespace std;
 #ifndef _RESOURCE_H
 #define _RESOURCE_H
-struct info{
-	int day; 
-	int month; 
-	int year;
-};
+#define N_MGZ 100
+typedef struct resrc_info{
+	string month;
+	string mem_name;
+	bool state;
+	string date;
+	string due_date;
+	int size;
+}INFO;
 
 class Resource{
 	private:
-		vector<string> name;
-		vector<string> mem_name;
-		vector<bool> state;
-		vector<struct info> date;
-		vector<struct info> due_date;
-		int location;
-		void print();
-		void set_date(string _date, int due);
-		void struct_to_string(struct info due, string &ret_date);
 	public:
-		void add_resource(string _name);
+		vector<string> name;
+		int location;
 		int isInLibrary(string _name);
-		int isAvailable(string member_name, string _date, string& ret_date);
-		void final_state(bool in, int due, string member_name, string _date);
-		int isGoodReturn(string member_name, string _date);
-		void get_due(string& d_day)
-		{
-			struct_to_string(due_date.at(location), d_day);
-		}
+		int isGoodReturn(string _date, string due_date);
+		void set_date(string _date, int due, string&, string&);
 };
 
 class Book : public Resource{
 	private:
+		vector<INFO> data;
+		int isAvailable(string member_name, string _date, string& ret_date);
 	public:
+		void final_state(bool in, int due, string member_name, string _date);
+		void add_resource(string _name);
 		int do_op(string B, string _name, string mem_name, string now, string &ret_date);
+		void get_due(string& d_day)
+		{
+			d_day = data.at(location).due_date;
+		}
 		
 };
+class Magazine : public Resource{
+	private:
+		vector<INFO*> data;
+		string month_rec;
+		int isAvailable(string member_name, string _date, string& ret_date, string month);
+		int check_month(string, string);
+	public:
+		void add_resource(string _name);
+		void final_state(bool in, int due, string member_name, string _date);
+		int do_op(string B, string _name, string mem_name, string now, string &ret_date);
+		void get_due(string& d_day)
+		{
+			int i;
+			for(i=0;i<N_MGZ;i++)
+			{
+				if(data.at(location)[i].month == month_rec) break;
+			}
+			d_day = data.at(location)[i].due_date;
+		}
+};
+
+/*class E_book : public Resource{
+	private:
+		vector<INFO> data;
+	public:
+		void add_resource(string _name, int size);
+};*/
 
 #endif
