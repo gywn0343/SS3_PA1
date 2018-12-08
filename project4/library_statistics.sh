f1294c260192b8c3016d676400a39c3c6e587812
gywn0343 <gywn0343@gmail.com>
2018-12-07 20:39:03 -0800

    done with stat_table.dat working on 'all' instruction

---
+++ project4/library_statistics.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo $1 $2 $3
+if [ "$1" == "resource" ] && [ "$2" != "all" ]; 
+then
+	#if [ $2="all"]
+	#then
+	cat $1.dat | grep -w 'Type' > tmp.dat
+	cat $1.dat | grep -w $2 >> tmp.dat
+	mkdir -p resource
+	mv -f tmp.dat ./resource
+	cd ./resource
+	mv tmp.dat $2.dat
+	cd ..
+fi
+if [ "$1" == "input" ] && ["$2" != "all"];
+then
+	x="resource.dat"
+	y="space.dat"
+	target="input"
+	option=$2
+elif [ "$1" == "space" ] && [ "$2" != "all" ]
+then
+	x="input.dat"
+	y="resource.dat"
+	target="space"
+	option=$2
+	if [ "$3" != '' ] && [ "$3" != 'all' ] && [ "$4" == '' ];
+	then
+		option=$2'\t'$3
+	fi
+	make
+	cat $1.dat | grep -w 'Date' > tmp.dat
+	cat $1.dat | grep -P $option >> tmp.dat
+	mkdir -p tmp_dir
+	mkdir -p $target
+	cp -f tmp.dat ./$target
+	cd ./$target
+	mv tmp.dat $2.dat
+	cd ..
+	mv -f tmp.dat ./tmp_dir
+	mv -f ./lib ./tmp_dir
+	cp -f $x $y ./tmp_dir
+	cd ./tmp_dir
+	mv tmp.dat $target.dat
+	./lib
+	cp -f output.dat ..
+	rm *
+	cd ..
+	#mkdir -p output
+	#mv -f output.dat output
+	rmdir tmp_dir
+#	fi
+fi
+
+if [ "$1" == "output" ]
+then
+	mkdir -p output
+	cp -f output.dat ./output
+	cd ./output
+	echo -e Return_code' \t'Number > stat_table.dat
+	for x in -1 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+	do
+		cnt=0
+		while read N R Des
+		do
+			if [ "$R" == "Return_code" ]
+			then
+				continue
+			elif [ $R -eq $x ]
+			then
+				((cnt++))
+			fi
+		done < output.dat
+		echo -e $x' \t'$cnt >> stat_table.dat
+	done
+fi
